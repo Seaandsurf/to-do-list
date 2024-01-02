0518c73b7fef7ade903807dbd5bdc977233cdc0f
Seaandsurf <mathias.hardat@gmail.com>
2024-01-02 16:57:44 +0100

    to do list

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,7 @@ function added() {
     let boxy = document.getElementById("box-id");
     let todo = document.getElementById("to-do");
     for (i = 0; i < myNodelist.length; i++) {
+        // Creation d'un <span class="close"> x <span>
         let span = document.createElement("SPAN");
         let txt = document.createTextNode("\u00D7");
         span.className = "close";
@@ -57,10 +58,11 @@ function addnew() {
     let t = document.createTextNode(inputs);
     li.appendChild(t);
     if (inputs === '') {
-        alert("Come on, there must be something you need to do!");
+        alert("Come on, On a TOUJOURS quelque chose a faire!");
     } else {
         document.getElementById("list").appendChild(li);
     }
+    
     document.getElementById("new").value = "";
     let span = document.createElement("SPAN");
     let txt = document.createTextNode("\u00D7");
@@ -80,17 +82,65 @@ function addnew() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//JSON SHIT -----------------------------------------
+
+
+// function saved() {
+//     const tab = [];
+//     document.querySelectorAll("box__list, li").forEach((li) => {
+//         tab.push({
+//             text: li.textContent,
+//             // completed: li.getElementsByClassName("complet"),
+//         });
+//     });
+//     localStorage.setItem("tab", JSON.stringify(tab));
+//   }
+
+//   function loadtab() {
+//     const tab = JSON.parse(localStorage.getItem("tab")) || [];
+//     tab.forEach((task) => {
+//         const li = document.createElement("li");
+//         li.textContent = task.text;
+//         list.appendChild(li);
+//     });
+//   }
+
+
+
+
 //active button with .active class
 
-$(function () {
-    $('.add_new').click(function () {
+// $(function () {
+//     $('.add_new').click(function () {
 
-        $(this).toggleClass('active');
-        setTimeout(function () {
-            $('.add_new').removeClass('active');
-        }, 1100);
-    });
-});
+//         $(this).toggleClass('active');
+//         setTimeout(function () {
+//             $('.add_new').removeClass('active');
+//         }, 1100);
+//     });
+// });
 
+
+
+
+//JSON SHIT -----------------------------------------
 
 
